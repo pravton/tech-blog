a21caba8d6e3cda86a434b37ef8f77ad31c5f3da
PravTon <clintonsf@outlook.com>
2021-07-23 18:48:27 -0400

    added helpers

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
