c734a01c45bfd428c47409ce6a0a9e7deb0860e2
PravTon <clintonsf@outlook.com>
2021-07-23 23:05:36 -0400

    fixed style issues and responsiveness

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
+//find popular posts based on comments
 router.get('/', (req, res) => {
   Post.findAll({
     order: [['created_at', 'DESC']],
@@ -23,8 +24,30 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({plain: true}));
       // pass a single post object into the homepage template
+      const totalPosts = [[], []];
+      if(posts.length > 5) {
+        // Get the latest 5 posts for slider
+        for(let i = 0; i < 5; i++) {
+          totalPosts[0].push(posts[i]);
+        }
+        // Get the rest of the posts for popular posts
+        for(let i = 6; i < posts.length; i++) {
+          totalPosts[1].push(posts[i]);
+        }
+      } else {
+        totalPosts[0].push(posts[0]);
+        for(let i = 1; i < posts.length; i++) {
+          totalPosts[1].push(posts[i]);
+        }
+      }
+
+      //sort the popular posts based on the comments 
+      totalPosts[1].sort(function(a, b) {
+        return b.comments.length - a.comments.length ;
+      });
+
       res.render('homepage', {
-        posts,
+        totalPosts,
         loggedIn: req.session.loggedIn
       });
     })
@@ -33,6 +56,42 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// find latest 5 content for slider
+// router.get('/', (req, res) => {
+//   Post.findAll({
+//     order: [['created_at', 'DESC']],
+//     limit: 5,
+//     include: [
+//       {
+//         model: Comment,
+//         order: [['created_at', 'DESC']],
+//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//         include: {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       },
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       const sliderPosts = dbPostData.map(post => post.get({plain: true}));
+//       console.log(sliderPosts);
+//       // pass a single post object into the homepage template
+//       res.render('homepage', {
+//         sliderPosts,
+//         loggedIn: req.session.loggedIn
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 // Get a single post
 router.get('/posts/:id', (req, res) => {
@@ -58,7 +117,6 @@ router.get('/posts/:id', (req, res) => {
   })
     .then(dbPostData => {
       const post = dbPostData.get({plain: true});
-      console.log(post);
       post.content = post.content.split("\n");
   
       // pass a single post object into the homepage template
